0393d7b56ff5a10f3a71302022cb3e01d1724028
probablysean <sms814@scarletmail.rutgers.edu>
2022-11-03 01:18:22 -0400

    Rhythm Game

--- Party Animalz 1.01/Assets/Scripts/GameManager.cs
+++ Party Animalz 1.01/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,13 +13,24 @@ public class GameManager : MonoBehaviour
 
     public static GameManager instance;
 
+    public Text scoreText;
+    public Text multiText;
+
     public int currentScore;
     public int scorePerNote = 100;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    private string score;
+    private string cM;
 
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        currentMultiplier = 1;
     }
 
     // Update is called once per frame
@@ -39,13 +51,35 @@ public class GameManager : MonoBehaviour
     public void NoteHit()
     {
         Debug.Log("hit on time");
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
 
-        currentScore += scorePerNote;
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+
+            }
+        }
+
+        cM = currentMultiplier.ToString();
+        multiText.text = "x" + cM;
+
+        currentScore += scorePerNote * currentMultiplier;
+
+        score = currentScore.ToString();
+        scoreText.text = score;
 
     }
 
     public void NoteMissed()
     {
         Debug.Log("Missed");
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+
+        cM = currentMultiplier.ToString();
+        multiText.text = "x" + cM;
     }
 }
