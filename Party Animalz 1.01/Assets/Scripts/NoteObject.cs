b2a58bbab5221faea82642cec86d5c5d36924124
probablysean <sms814@scarletmail.rutgers.edu>
2022-11-05 17:08:09 -0400

    rr

--- Party Animalz 1.01/Assets/Scripts/NoteObject.cs
+++ Party Animalz 1.01/Assets/Scripts/NoteObject.cs
@@ -11,6 +11,8 @@ public class NoteObject : MonoBehaviour
 
     private bool obtained = false;
 
+    public GameObject hitEffect, goodEffect, perfectEffect, missedEffect;
+
 
 
     // Start is called before the first frame update
@@ -29,20 +31,37 @@ public class NoteObject : MonoBehaviour
                 
                 //GameManager.instance.NoteHit();
 
-                if(transform.position.y > -3.60 || transform.position.y < -3.90)
+                if(transform.position.y > -3.69 || transform.position.y < -3.81)
                 { 
                     GameManager.instance.NormalHit();
                     Debug.Log("Nice");
+                    //Instantiate(hitEffect, transform.position, hitEffect.transform.rotation) ;
+                    GameObject e1 = Instantiate(hitEffect, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                    e1.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+                    e1.transform.position = transform.position;
+                    e1.transform.rotation = hitEffect.transform.rotation;
+
+
                 }
-                else if(transform.position.y > -3.72 || transform.position.y < -3.78)
+                else if(transform.position.y > -3.73 || transform.position.y < -3.77)
                 {
                     GameManager.instance.GoodHit();
                     Debug.Log("Good");
+                    //Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+                    GameObject e1 = Instantiate(goodEffect, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                    e1.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+                    e1.transform.position = transform.position;
+                    e1.transform.rotation = hitEffect.transform.rotation;
                 }
                 else
                 {
                     GameManager.instance.PerfectHit();
                     Debug.Log("Perfect");
+                    //Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                    GameObject e1 = Instantiate(perfectEffect, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                    e1.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+                    e1.transform.position = transform.position;
+                    e1.transform.rotation = perfectEffect.transform.rotation;
                 }
             }
         }
@@ -64,6 +83,11 @@ public class NoteObject : MonoBehaviour
             if(!obtained)
             {
                 GameManager.instance.NoteMissed();
+                //Instantiate(missedEffect, transform.position, missedEffect.transform.rotation);
+                GameObject e1 = Instantiate(missedEffect, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                e1.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+                e1.transform.position = transform.position;
+                e1.transform.rotation = missedEffect.transform.rotation;
             }
         }
     }
