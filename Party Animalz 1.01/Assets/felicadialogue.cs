45e7d0275fd84ec3f421b3bdc7453a9d5f7f8eed
janani090 <115048302+janani090@users.noreply.github.com>
2022-12-06 20:03:11 -0500

    felicapt2

---
+++ Party Animalz 1.01/Assets/felicadialogue.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class felicadialogue : MonoBehaviour
+{
+    public PlayerDialogObject PDO;
+    public DialogInteractObject DIO;
+    public int slide1;
+    public Sprite[] dialogOne;
+    public GameObject mainImage;
+    private Sprite image;
+    public GameObject mainButton;
+
+    public SceneHandler sceneHandler;
+    public bool dialogStarted;
+
+
+    void Awake()
+    {
+        PDO = GameObject.Find("PlayerDialogObject").GetComponent<PlayerDialogObject>();
+        dialogStarted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e") && DIO.isTalkingDIO == true && dialogStarted == true)
+        {
+            AddSlide();
+        }
+
+        //start dialog skip bug correction
+        if (DIO.isTalkingDIO == true && dialogStarted == false)
+        {
+            dialogStarted = true;
+            AddSlide();
+        }
+    }
+
+    public void AddSlide()
+    {
+        slide1 += 1;
+
+        mainImage.GetComponent<Image>().sprite = dialogOne[slide1];
+
+        if (slide1 == 1)
+        {
+            mainImage.SetActive(true);
+            mainButton.SetActive(true);
+        }
+
+        if (slide1 == 3)
+        {
+            EndDialog();
+            DIO.CloseDialog();
+        }
+    }    
+
+    void EndDialog()
+    {
+        DIO.EndDialog();
+        PDO.EndDialog();
+
+        mainButton.SetActive(false);
+        mainImage.SetActive(false);
+
+        dialogStarted = false;
+    }
+}
